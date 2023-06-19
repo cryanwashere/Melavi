2b47317610c68c770f5993680abad3282a8c0fb8
Cam <cjryanwashere@gmail.com>
2023-06-19 11:46:04 -0400

    Initial Commit

---
+++ Melavi/MelaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelaviApp.swift
+//  Melavi
+//
+//  Created by CJ Ryan on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MelaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
